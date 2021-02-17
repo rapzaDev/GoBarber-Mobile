a8273c4fd617505d957d05eabcce2d05be56275c
Rafael Pérez <rapzadev@gmail.com>
2021-02-16 21:17:15 -0300

    Formatting Success Page Date

--- src/pages/AppointmentCreated/index.tsx
+++ src/pages/AppointmentCreated/index.tsx
@@ -1,5 +1,7 @@
-import { useNavigation } from '@react-navigation/native';
-import React, { useCallback } from 'react';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { format } from 'date-fns';
+import enUS from 'date-fns/locale/en-US';
+import React, { useCallback, useMemo } from 'react';
 import Icon from 'react-native-vector-icons/Feather';
 
 import {
@@ -10,8 +12,23 @@ import {
   OkButtonText,
 } from './styles';
 
+interface RouteParams {
+  date: number;
+}
+
 const AppointmentCreated: React.FC = () => {
   const { reset } = useNavigation();
+  const { params } = useRoute();
+
+  const routeParams = params as RouteParams;
+
+  const formattedDate = useMemo(() => {
+    return format(
+      routeParams.date,
+      "EEEE dd 'of' MMMM 'of' yyyy 'at' HH:mm'h'",
+      { locale: enUS },
+    );
+  }, [routeParams.date]);
 
   const handleOkPressed = useCallback(() => {
     reset({
@@ -25,7 +42,7 @@ const AppointmentCreated: React.FC = () => {
       <Icon name="check" size={80} color="#04d361" />
 
       <Title>Appointment concluded</Title>
-      <Description>Description of appointment</Description>
+      <Description>{formattedDate}</Description>
 
       <OkButton onPress={handleOkPressed}>
         <OkButtonText>Ok</OkButtonText>
